38f1b62b21b2a400491ad15c298e85feeda0bf09
agloba <a.globa@otdelit.ru>
2020-08-01 08:48:22 +0500

    Sync commit

--- gotspl/command_filemgmt_eop.go
+++ gotspl/command_filemgmt_eop.go
@@ -15,45 +15,23 @@
  */
 package gotspl
 
-import (
-	"bytes"
-	"errors"
-	"strings"
-)
+import "bytes"
 
-const RUN_NAME = "RUN"
+const EOP_NAME = "EOP"
 
-type RunImpl struct {
-	file *string
+type EopImpl struct {
 }
 
-type RunBuilder interface {
+type EopBuilder interface {
 	TSPLCommand
-	File(name string) RunBuilder
 }
 
-func RunCmd() RunBuilder {
-	return RunImpl{}
+func EopCmd() HomeBuilder {
+	return EopImpl{}
 }
 
-func (r RunImpl) GetMessage() ([]byte, error) {
-	if r.file == nil || len(*r.file) == 0 {
-		return nil, errors.New("ParseError RUN Command: file should be specified")
-	}
-
-	buf := bytes.NewBufferString(RUN_NAME)
-	buf.WriteString(EMPTY_SPACE)
-	buf.WriteString(DOUBLE_QUOTE)
-	buf.WriteString(strings.ToUpper(*r.file))
-	buf.WriteString(DOUBLE_QUOTE)
+func (h EopImpl) GetMessage() ([]byte, error) {
+	buf := bytes.NewBufferString(EOP_NAME)
 	buf.Write(LINE_ENDING_BYTES)
 	return buf.Bytes(), nil
-}
-
-func (r RunImpl) File(name string) RunBuilder {
-	if r.file == nil {
-		r.file = new(string)
-	}
-	*r.file = name
-	return r
 }
---
+++ gotspl/commang_filemgmt_download.go
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2020 Anton Globa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gotspl
+
+import (
+	"bytes"
+	"errors"
+)
+
+const (
+	DOWNLOAD_NAME                                     = "DOWNLOAD"
+	DOWNLOAD_STORGAE_DRAM             DownloadStorage = ""
+	DOWNLOAD_STORAGE_FLASH            DownloadStorage = "F"
+	DOWNLOAD_STORAGE_EXPANSION_MODULE DownloadStorage = "E"
+)
+
+type DownloadStorage string
+
+type DownloadImpl struct {
+	storage *string
+	name    *string
+}
+
+type DownloadBuilder interface {
+	TSPLCommand
+	Storage(storage DownloadStorage) DownloadBuilder
+	Name(name string) DownloadBuilder
+}
+
+func DownloadCmd() DownloadBuilder {
+	return DownloadImpl{}
+}
+
+func (d DownloadImpl) GetMessage() ([]byte, error) {
+
+	if d.name == nil || len(*d.name) == 0 {
+		return nil, errors.New("ParseError DOWNLOAD Command: name should be specified")
+	}
+
+	if d.storage == nil {
+		d.storage = new(string)
+		*d.storage = string(DOWNLOAD_STORGAE_DRAM)
+	}
+
+	buf := bytes.NewBufferString(DOWNLOAD_NAME)
+	buf.WriteString(EMPTY_SPACE)
+	buf.WriteString(*d.storage)
+	buf.WriteString(VALUE_SEPARATOR)
+	buf.WriteString(DOUBLE_QUOTE)
+	buf.WriteString(*d.name)
+	buf.WriteString(DOUBLE_QUOTE)
+	buf.Write(LINE_ENDING_BYTES)
+	return buf.Bytes(), nil
+}
+
+func (d DownloadImpl) Storage(storage DownloadStorage) DownloadBuilder {
+	if d.storage == nil {
+		d.storage = new(string)
+	}
+	*d.storage = string(storage)
+	return d
+}
+
+func (d DownloadImpl) Name(name string) DownloadBuilder {
+	if d.name == nil {
+		d.name = new(string)
+	}
+	*d.name = name
+	return d
+}
